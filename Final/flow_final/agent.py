5ec1600089ebf5429097da53c6565f44bde8f61b
이재원 (Jaewon Lee) <wodnjsdl0123@naver.com>
2025-07-20 17:55:31 +0900

    Delete Final/flow_final/agent.py

--- Final/flow_final/agent.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-
-# 환경변수 설정
-load_dotenv()
-
-# 베이스 LLM 설정
-model = ChatOpenAI(temperature=0.2, model = "gpt-4o") # 보수적인 답변을 위해 temperature는 0.2로 제한
